b364c7d4af43298e1d04282a3b05c14d45abd033
ParaDox <doniyorjorabekov@gmail.com>
2024-03-05 09:22:32 +0500

    player cards screen change showDialog animation

--- lib/src/presentation/pages/main/player_cards/widgets/player_cards_list.dart
+++ lib/src/presentation/pages/main/player_cards/widgets/player_cards_list.dart
@@ -33,48 +33,52 @@ Future<void> showPlayerCardsImage(
     showDialog<Future<void>>(
       context: context,
       builder: (context) => index.isEven
-          ? SlideInLeft(
-              child: GestureDetector(
-                onTap: () => context.pop(),
-                child: CachedNetworkImage(
-                  imageUrl: image ?? 'null',
-                  placeholder: (context, url) => Center(
-                    child: StaggeredDotsWave(
-                      size: 50,
-                      color: context.colorScheme.secondary,
+          ? SlideInDown(
+            child: SlideInLeft(
+                child: GestureDetector(
+                  onTap: () => context.pop(),
+                  child: CachedNetworkImage(
+                    imageUrl: image ?? 'null',
+                    placeholder: (context, url) => Center(
+                      child: StaggeredDotsWave(
+                        size: 50,
+                        color: context.colorScheme.secondary,
+                      ),
                     ),
-                  ),
-                  fadeOutDuration: const Duration(milliseconds: 300),
-                  errorWidget: (context, url, error) => Center(
-                    child: Icon(
-                      Icons.image_not_supported_outlined,
-                      color: colorLightScheme.secondary,
-                      size: 50.w,
-                    ),
-                  ),
-                ),
-              ),
-            )
-          : SlideInRight(
-              child: GestureDetector(
-                onTap: () => context.pop(),
-                child: CachedNetworkImage(
-                  imageUrl: image ?? 'null',
-                  placeholder: (context, url) => Center(
-                    child: StaggeredDotsWave(
-                      size: 50,
-                      color: context.colorScheme.secondary,
-                    ),
-                  ),
-                  fadeOutDuration: const Duration(milliseconds: 300),
-                  errorWidget: (context, url, error) => Center(
-                    child: Icon(
-                      Icons.image_not_supported_outlined,
-                      color: colorLightScheme.secondary,
-                      size: 50.w,
+                    fadeOutDuration: const Duration(milliseconds: 300),
+                    errorWidget: (context, url, error) => Center(
+                      child: Icon(
+                        Icons.image_not_supported_outlined,
+                        color: colorLightScheme.secondary,
+                        size: 50.w,
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
+          )
+          : SlideInUp(
+            child: SlideInRight(
+                child: GestureDetector(
+                  onTap: () => context.pop(),
+                  child: CachedNetworkImage(
+                    imageUrl: image ?? 'null',
+                    placeholder: (context, url) => Center(
+                      child: StaggeredDotsWave(
+                        size: 50,
+                        color: context.colorScheme.secondary,
+                      ),
+                    ),
+                    fadeOutDuration: const Duration(milliseconds: 300),
+                    errorWidget: (context, url, error) => Center(
+                      child: Icon(
+                        Icons.image_not_supported_outlined,
+                        color: colorLightScheme.secondary,
+                        size: 50.w,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+          ),
     );
